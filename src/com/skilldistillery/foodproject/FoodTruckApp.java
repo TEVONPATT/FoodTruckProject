4b8d6a686dc0f0f77d0427cd83bec096615045c0
Tevon Patterson <Tevonpatt@gmail.com>
2020-09-08 01:24:29 -0700

    small adjustments

--- src/com/skilldistillery/foodproject/FoodTruckApp.java
+++ src/com/skilldistillery/foodproject/FoodTruckApp.java
@@ -13,7 +13,6 @@ public class FoodTruckApp {
 	private void run(Scanner scanner) {
 		FoodTruck[] trucks = new FoodTruck[5];
 		buildUserTruck(scanner, trucks);
-//		trucks = getTestTruckArr();
 		boolean keepGoing = true;
 		while (keepGoing) {
 			System.out.println("       __________________________________________________ ");
@@ -48,9 +47,6 @@ public class FoodTruckApp {
 
 		}
 
-//		printTruck(trucks);
-//		averageRating(trucks);
-//		highestRating(trucks);
 	}
 
 	private void printTruck(FoodTruck[] trucks) {
@@ -77,14 +73,6 @@ public class FoodTruckApp {
 
 	private void highestRating(FoodTruck[] trucks) {
 		double countTrucks = 0;
-//		for(int num : numbers) {
-//			if(num > max) {
-//				max = num;
-//			}
-//		}
-//		if(numbers[i] > max) {
-//			max = numbers[i];
-//		}
 
 		FoodTruck forRatings = null;
 		double highest = 0;
@@ -111,54 +99,9 @@ public class FoodTruckApp {
 			String foodType = scanner.nextLine();
 			System.out.println("Enter in the food rating from 1 to 5: ");
 			double rating = scanner.nextDouble();
-//		displayUserRating(rating);
 			scanner.nextLine();
 			FoodTruck truck = new FoodTruck(truckName, foodType, rating);
 			trucks[i] = truck;
 		}
-	}
-
-	private void displayUserRating(int rating) {
-		switch (rating) {
-		case 0:
-			System.out.println("You have given this food truck 0 out of 5 stars.");
-			break;
-		case 1:
-			System.out.println("You have given this food truck 1 out of 5 stars.");
-			break;
-		case 2:
-			System.out.println("You have given this food truck 2 out of 5 stars.");
-			break;
-		case 3:
-			System.out.println("You have given this food truck 3 out of 5 stars.");
-			break;
-		case 4:
-			System.out.println("You have given this food truck 4 out of 5 stars.");
-			break;
-		case 5:
-			System.out.println("You have given this food truck 5 out of 5 stars.");
-			break;
-		default:
-			break;
-		}
-	}
-
-	private FoodTruck[] getTestTruckArr() {
-		FoodTruck truck1 = new FoodTruck();
-		truck1.setTruckName("house");
-		truck1.setFoodType("pancackes");
-		truck1.setRating(4);
-
-		FoodTruck truck2 = new FoodTruck();
-		truck2.setTruckName("turf");
-		truck2.setFoodType("SeasFood");
-		truck2.setRating(2);
-
-		FoodTruck truck3 = new FoodTruck();
-		truck3.setTruckName("Food");
-		truck3.setFoodType("Mexican Style");
-		truck3.setRating(5);
-		FoodTruck[] truckArr = { truck1, truck2, truck3 };
-		return truckArr;
 	}
 }
